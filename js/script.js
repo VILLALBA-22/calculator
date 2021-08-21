b0a5e33ae52e0adef320673cac8833c9ce94c9ba
Andres Villalba <andresfelipevillalba486@gmail.com>
2021-08-20 21:08:22 -0500

    Adding funtion to change theme

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 const d = document
 const result = d.querySelector('.result-p')
 const $circle_option = d.querySelector('.circle-option')
-
 const keyValue = (typeKey, e) => {
 	if (e.target.matches(typeKey)) {
 		result.textContent += d.querySelector(typeKey).textContent
 	}
 }
-
 d.addEventListener('DOMContentLoaded', () => {
 	let arrive =
 		d.querySelector('.option3').getBoundingClientRect().x -
 		$circle_option.getBoundingClientRect().x
+	if (localStorage.getItem('theme')) {
+		d.body.classList.add(localStorage.getItem('theme'))
+		$circle_option.style.transform = localStorage.getItem('translateX')
+	}
 	d.addEventListener('click', e => {
 		keyValue('.key0', e)
 		keyValue('.key1', e)
@@ -40,16 +42,25 @@ d.addEventListener('DOMContentLoaded', () => {
 			)
 			d.querySelector('.result-p').textContent = result_process || 'Error'
 		}
-
 		// Chose Theme
-		if (e.target.matches('.option3')) {
-			$circle_option.style.transform = `translateX(${arrive + 4}px)`
+		if (e.target.matches('.option1')) {
+			d.body.classList.remove('theme-2', 'theme-3')
+			$circle_option.style.transform = `translateX(0px)`
+			localStorage.removeItem('theme')
 		}
 		if (e.target.matches('.option2')) {
 			$circle_option.style.transform = `translateX(${(arrive + 2) / 2}px)`
+			d.body.classList.add('theme-2')
+			d.body.classList.remove('theme-3')
+			localStorage.setItem('translateX', `translateX(${(arrive + 2) / 2}px)`)
+			localStorage.setItem('theme', 'theme-2')
 		}
-		if (e.target.matches('.option1')) {
-			$circle_option.style.transform = `translateX(0px)`
+		if (e.target.matches('.option3')) {
+			$circle_option.style.transform = `translateX(${arrive + 4}px)`
+			d.body.classList.add('theme-3')
+			d.body.classList.remove('theme-2')
+			localStorage.setItem('translateX', `translateX(${arrive + 4}px)`)
+			localStorage.setItem('theme', 'theme-3')
 		}
 	})
 })
